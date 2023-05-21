6077379d9b6f12b39f939e18fdf1bbe43580f87d
tommyboiiii004 <thomastharun004@gmail.com>
2023-05-21 14:28:33 +0530

    fixed small errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import React from 'react'
 import styles from './style';
-
+import { Navbar, Hero } from './components';
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <div className={'${styles.paddingX} ${styles.flexcenter}'}>
-        <div className={'${styles.boxwidth}'}>
-          <Navbar /> 
-        </div>
-      </div>
-
-      <div className={'bg-primary ${styles.flexstart}'}>
-        <div className={'${styles.boxwidth}'}>
-          <Hero />
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
         </div>
       </div>
 
     </div>
   )
-  
+
 }
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import styles from '../style'
-import {Discount , robot } from '../assets';
 
-const Hero = () => (
-
-  /*<section id="home" className={'flex md:flex-row flex-col '}>
-    <div className={'flex-1 ${styles.flexstart}flex-col xl:px-0 sm:px-16 px-6'}>
-      <div className="flex flex-row items.center py.[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-        <img src={robot} alt="discount"
-         className="w=[32px] h-[32px]" />
-        
-      </div>
-      
-    </div>
-
-     
-      
-  </section>*/
-  <img src={robot} alt="discount"
-  className="w=[32px] h-[32px]" />
+const Hero = () => {
+  return (
+    <div>Hero</div>
   )
-  
+}
 
 export default Hero
 
